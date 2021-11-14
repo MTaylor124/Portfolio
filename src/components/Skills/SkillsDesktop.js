c3d72996157cf2138bfac585b7c1f71404ac2524
MTaylor124 <matthew_taylor1@student.uml.edu>
2021-11-13 23:43:04 -0500

    Further refactored skills section - replaced divs with semantically named elements

--- src/components/Skills/SkillsDesktop.js
+++ src/components/Skills/SkillsDesktop.js
@@ -1,21 +1,20 @@
 import React from 'react'
-
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
 import { includedSkills } from "./SkillsData"
 
 export const SkillsDesktop = () => {
     const accordion = { backgroundColor: 'rgba(0, 118, 133, 0)', width: '65%', boxShadow: 'none', border: 'none', minHeight: '0px' }
-    const summary = { padding: '0px',  minHeight: '10px', fontSize: '1.8rem', textDecorationColor: 'rgb(0, 69, 133)' }
+    const summary = { padding: '0px',  minHeight: '10px', fontSize: '1.8rem', textDecorationColor: 'rgb(0, 69, 133)', backgroundColor: 'rgba(255, 255, 255, 0.25)' }
     const details = { boxShadow: 'none', border: 'none', paddingTop: '0' }
 
     const skillUnderlineList = includedSkills.map(item => false)
 
     const handleToggleUnderline = (i, id) => {
-        if (!skillUnderlineList[i]) document.getElementById(`${id}-header`).style.textDecoration = 'underline'
-        else document.getElementById(`${id}-header`).style.textDecoration = 'none'
+        const nodeStyle = document.getElementById(`${id}-header`).style
+        if (!skillUnderlineList[i]) nodeStyle.textDecoration = 'underline'
+        else nodeStyle.textDecoration = 'none'
         skillUnderlineList[i] = !skillUnderlineList[i]
     }
 
@@ -25,16 +24,17 @@ export const SkillsDesktop = () => {
                 <header>Skills</header>
                 <p>Click to Expand</p>
                 {includedSkills.map((skill, i) => (
-                    <Accordion style={accordion} key={skill.id} onChange={() => handleToggleUnderline(i, skill.id)} sx={{'&:before': {display: 'none'}}} disableGutters TransitionProps={{ unmountOnExit: true }}>
-                        <AccordionSummary style={summary} aria-controls={`panel${i}-content`} id={`panel${i}-header`} className='skill-underline'>
-                            <header id={`${skill.id}-header`}>{skill.buttonText}</header>
+                    <Accordion style={accordion} key={skill.id} onChange={() => handleToggleUnderline(i, skill.id)} sx={{ '&:before': { display: 'none' }}} disableGutters TransitionProps={{ unmountOnExit: true }}>
+                        <AccordionSummary style={summary} aria-controls={`panel${i}-content`} id={`panel${i}-header`}>
+                            <div className='skill-underline'><header id={`${skill.id}-header`}>{skill.buttonText}</header></div>
                         </AccordionSummary>
                         <AccordionDetails style={details}>
+                            {skill.about ? <h2>{skill.about}</h2> : <></>}
                             {skill.sections.map(section => (
-                                <div className="d-skills-details-container" key={section.id}>
-                                    <div className="d-skills-details-header">{section.header}</div>
-                                    {section.points.map(point => <div key={point.id} className="d-skills-info">{point.body}</div>)}
-                                </div>
+                                <section key={section.id}>
+                                    <h3>{section.header}</h3>
+                                    {section.points.map(point => <h4 key={point.id}>{point.body}</h4>)}
+                                </section>
                             ))}
                         </AccordionDetails>
                     </Accordion>
