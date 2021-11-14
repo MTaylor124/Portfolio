48609c3e9be23f7238c3293f5161410ca2ba9bf2
MTaylor124 <matthew_taylor1@student.uml.edu>
2021-11-14 02:51:32 -0500

    Created NavData file

---
+++ src/components/Nav/NavData.js
@@ -0,0 +1,7 @@
+export const navData = [
+    { text: 'Overview', id: 'nav-link-item-000', link: '/' },
+    { text: 'Projects', id: 'nav-link-item-001', link: '/projects' },
+    { text: 'Experience', id: 'nav-link-item-002', link: '/experience' },
+    { text: 'Skills', id: 'nav-link-item-003', link: '/skills' },
+    { text: 'Contact', id: 'nav-link-item-004', link: '/contact' }
+]
