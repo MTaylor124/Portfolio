470a84c72eea577bfb52fdd90437e8c244d92cbc
MTaylor124 <matthew_taylor1@student.uml.edu>
2021-11-18 21:57:32 -0500

    Updated margin for accordions

--- src/Desktop/Skills/SkillsDesktop.js
+++ src/Desktop/Skills/SkillsDesktop.js
@@ -6,7 +6,7 @@ import { includedSkills } from "../../Data/SkillsData"
 
 export const SkillsDesktop = () => {
     const accordion = { backgroundColor: 'rgba(0, 118, 133, 0)', width: '65%', boxShadow: 'none', border: 'none', minHeight: '0px' }
-    const summary = { padding: '0px',  minHeight: '10px', fontSize: '1.8rem', backgroundColor: 'rgba(255, 255, 255, 0.25)' }
+    const summary = { padding: '0px',  minHeight: '10px', fontSize: '1.8rem', backgroundColor: 'rgba(255, 255, 255, 0.25)', margin: '0px' }
     const details = { boxShadow: 'none', border: 'none', paddingTop: '0' }
 
     const skillUnderlineList = includedSkills.map(item => false)
